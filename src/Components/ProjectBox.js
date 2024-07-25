53c81a16a3eda52f859ebf8f15aed0ee5c98bec8
siddiqui0013 <asifm8702@gmail.com>
2024-07-25 12:06:25 +0500

    Updated links

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -4,22 +4,22 @@ import { CiLink } from "react-icons/ci";
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
     EmployeeEaseDesc : "This website is created using React and Tailwind CSS, and user Authentication with Firebase. A Job Portal",
-    EmployeeEaseGithub : "https://github.com/siddiqui0013/employeease",
+    EmployeeEaseGithub : "https://github.com/siddiqui0013/employe_ease",
 
     ToDoListDesc : "This website is created using React. A to-To list",
     ToDoListGithub : "https://github.com/siddiqui0013/todo-list",
 
     eDukan69Desc : "This website is created using HTML, CSS and JavaScript and PHP for the backend. It is an e-commerce site demo.",
-    eDukan69Github : "https://github.com/siddiqui0013/eDukan",
+    eDukan69Github : "https://edukan69.netlify.app/",
 
     CeaserCipherDesc : "This website is created with HTML, CSS, Javascript, to enctypt/decrypt Ceaser Cipher text.",
     CeaserCipherGithub : "https://github.com/siddiqui0013/ceaser",
 
     GamaiynDesc : "This website is created using bootstrap. It is a online gaming site demo.",
-    GamaiynGithub : "https://github.com/siddiqui0013/gamaiyn",
+    GamaiynGithub : "https://gaimayn.netlify.app/",
 
     Attendance_ManagementDesc : "This is a full stack Attendence Management System (MERN) with user and admin panel.",
-    Attendance_ManagementGithub : "https://github.com/siddiqui0013/attentance-system",
+    Attendance_ManagementGithub : "https://github.com/siddiqui0013/attendance-system",
 
   }
 
