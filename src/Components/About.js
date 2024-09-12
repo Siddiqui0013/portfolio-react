56c4005109d1a560af4a32b3f1b7aea9b5304692
siddiqui0013 <asifm8702@gmail.com>
2024-09-12 14:09:34 +0500

    Updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,11 +10,14 @@ const About = () => {
       <div className='AboutPage'>
         <div className='AboutText'>
           <h1 className='AboutTextHeading' >About myself </h1>
-          <p>
+          {/* <p>
           A frest graduate in <b>Information Technology</b>, with expertise in Web development using <b>React Js.</b> With years of experience in developing web apps.I have also worked with CSS libraries like <b>Tailwind</b> and <b>Bootstrap. </b>
 I also have experience in integerating backend using Php and Google Firebase.
 Other then this, been doing projects for different platforms in different languages.
-          </p>
+          </p> */}
+
+<p> I hold a Bachelor's of Science in <b>Information Technology</b> and am a skilled software engineer with expertise in web development using <b>React</b> and <b>Next.js</b>. With years of experience building web applications, I am proficient in CSS libraries like <b>Tailwind</b> and <b>Bootstrap</b>, and have experience integrating backends using <b>PHP</b> and <b>Google Firebase</b>. I enjoy working with a wide range of technologies and programming languages, constantly exploring new platforms and solving complex challenges. </p>
+
         </div>
 
         <div>
